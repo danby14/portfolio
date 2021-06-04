3460a9a058005252e0d52b5e51b91e3ae5236951
danby14 <51226470+danby14@users.noreply.github.com>
2021-06-03 23:37:30 -0500

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,5 +17,15 @@ module.exports = {
         include: /assets/,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-E0F8Y5J3YB",
+          // "GA-TRACKING_ID", // Google Analytics / GA
+        ],
+      },
+    },
   ],
 }
